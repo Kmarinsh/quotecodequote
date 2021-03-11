e501cc55ef950e7f977d82ceac7d2111251f683f
Joseph Ortiz <jortiz23@verizon.net>
2021-03-10 21:20:27 -0800

    Create file structure for Project assignment 2

---
+++ src/compiler.js
@@ -0,0 +1,13 @@
+import parse from "./parser.js"
+// import analyze from "./analyzer.js"
+// import optimize from "./optimizer.js"
+// import generate from "./generator/index.js"
+
+export default function compile(source, outputType) {
+  outputType = outputType.toLowerCase()
+  if (outputType == "ast") {
+    return parse(source)
+  } else {
+    return "Unknown output type"
+  }
+}
